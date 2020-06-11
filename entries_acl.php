3d437fae75ca55a70b397f66111fca3c18bac197
Awilum <awilum@msn.com>
2020-06-11 15:43:46 +0300

    feat(accounts-controller): continue ACL implementation with twig and shortocdes

--- entries_acl.php
+++ entries_acl.php
@@ -8,6 +8,18 @@ $flextype->emitter->addListener('onEntryAfterInitialized', function() use ($flex
 
     $entry = $flextype->entries->entry;
 
+    if (isset($entry['access']['accounts']['uuids'])) {
+        if (!in_array(Session::get('account_uuid'), $entry['access']['accounts']['uuid'])) {
+            $flextype->entries->entry = [];
+        }
+    }
+
+    if (isset($entry['access']['accounts']['usernames'])) {
+        if (!in_array(Session::get('account_username'), $entry['access']['accounts']['usernames'])) {
+            $flextype->entries->entry = [];
+        }
+    }
+
     if (isset($entry['access']['accounts']['roles'])) {
         if (!in_array(Session::get('account_role'), $entry['access']['accounts']['roles'])) {
             $flextype->entries->entry = [];
