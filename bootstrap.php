8c671349ffa688e0d21b7d44211f25a51953f014
Awilum <awilum@msn.com>
2020-06-12 20:52:49 +0300

    feat(accounts-controller): add PHPMailer for registration

--- bootstrap.php
+++ bootstrap.php
@@ -11,6 +11,9 @@ declare(strict_types=1);
 
 namespace Flextype;
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use Flextype\Component\Filesystem\Filesystem;
 use function is_file;
 
 /**
