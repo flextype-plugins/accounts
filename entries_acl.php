30f889ab6d10a7c505c2a56a2cdd2c58d8aa379e
Awilum <awilum@msn.com>
2020-06-12 20:52:58 +0300

    feat(core): add multi roles support

--- entries_acl.php
+++ entries_acl.php
@@ -9,19 +9,24 @@ $flextype->emitter->addListener('onEntryAfterInitialized', function() use ($flex
     $entry = $flextype->entries->entry;
 
     if (isset($entry['access']['accounts']['uuids'])) {
-        if (!in_array(Session::get('account_uuid'), $entry['access']['accounts']['uuid'])) {
+        if (!in_array(Session::get('account_uuid'), array_map('trim', explode(',', $entry['access']['accounts']['uuid'])))) {
             $flextype->entries->entry = [];
         }
     }
 
     if (isset($entry['access']['accounts']['usernames'])) {
-        if (!in_array(Session::get('account_username'), $entry['access']['accounts']['usernames'])) {
+        if (!in_array(Session::get('account_username'), array_map('trim', explode(',', $entry['access']['accounts']['usernames'])))) {
             $flextype->entries->entry = [];
         }
     }
 
     if (isset($entry['access']['accounts']['roles'])) {
-        if (!in_array(Session::get('account_role'), $entry['access']['accounts']['roles'])) {
+        $entries_user_roles   = array_map('trim', explode(',', $entry['access']['accounts']['roles']));
+        $logged_in_user_roles = array_map('trim', explode(',', Session::get('account_roles')));
+
+        $result = array_intersect($entries_user_roles, $logged_in_user_roles);
+
+        if (empty($result)) {
             $flextype->entries->entry = [];
         }
     }
